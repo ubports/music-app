f11e6443d682e7135556037305b7e39eb74a0e25
nskaggs <nicholas.skaggs@canonical.com>
2013-10-09 14:04:17 -0400

    add setup, enforce app loads with and uses uitk emulator :-)

--- tests/autopilot/music_app/tests/__init__.py
+++ tests/autopilot/music_app/tests/__init__.py
@@ -20,7 +20,7 @@ from autopilot.platform import model
 from autopilot.testcase import AutopilotTestCase
 
 from ubuntuuitoolkit import emulators as toolkit_emulators
-from music_app.emulators import MainView
+from music_app import emulators
 
 logger = logging.getLogger(__name__)
 
@@ -57,14 +57,16 @@ class MusicTestCase(AutopilotTestCase):
         self.app = self.launch_test_application(
             "qmlscene",
             self.local_location,
-            app_type='qt')
+            app_type='qt',
+            emulator_base=toolkit_emulators.UbuntuUIToolkitEmulatorBase)
 
     def launch_test_installed(self):
         self.app = self.launch_test_application(
             "qmlscene",
             self.installed_location,
             "--desktop_file_hint=/usr/share/applications/music-app.desktop",
-            app_type='qt')
+            app_type='qt',
+            emulator_base=toolkit_emulators.UbuntuUIToolkitEmulatorBase)
 
     def launch_test_click(self):
         self.app = self.launch_click_package(
@@ -160,7 +162,6 @@ class MusicTestCase(AutopilotTestCase):
         os.remove(in_filename)
         os.rename(out_filename, in_filename)
 
-
     @property
     def main_view(self):
-        return MainView(self.app)
+        return self.app.select_single(emulators.MainView)
