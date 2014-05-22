da5fc692d297c8ef1804d257b2138f34bdb847e8
Andrew Hayzen <ahayzen@gmail.com>
2014-05-22 10:07:38 +0100

    * Fixes for Pep8

--- tests/autopilot/music_app/emulators.py
+++ tests/autopilot/music_app/emulators.py
@@ -83,9 +83,11 @@ class MainView(toolkit_emulators.MainView):
         return self.wait_select_single("*", objectName="genreItemObject")
 
     def get_back_button(self):
-        backButton = self.select_single("AbstractButton", objectName="customBackButton")
-        if (backButton.visible == False):
-            backButton = self.select_single("AbstractButton", objectName="backButton")
+        backButton = self.select_single("AbstractButton",
+                                        objectName="customBackButton")
+        if (backButton.visible is False):
+            backButton = self.select_single("AbstractButton",
+                                            objectName="backButton")
         return backButton
 
     def get_albumstab(self):
