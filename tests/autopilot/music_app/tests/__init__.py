aa1c474aadc2203f0da20c9c292661a3d2e1ce31
nskaggs <nicholas.skaggs@canonical.com>
2014-05-29 09:24:40 -0400

    simplify logic per thomi for local_location_qml

--- tests/autopilot/music_app/tests/__init__.py
+++ tests/autopilot/music_app/tests/__init__.py
@@ -43,9 +43,7 @@ class MusicTestCase(AutopilotTestCase):
     else:
         scenarios = [('with touch', dict(input_device_class=Touch))]
 
-    working_dir = os.getcwd()
-    local_location_dir = os.path.dirname(os.path.dirname(working_dir))
-    local_location = local_location_dir + "/music-app.qml"
+    local_location_qml = os.path.join(os.getcwd(), '..', '..', 'music-app.qml')
     installed_location = "/usr/share/music-app/music-app.qml"
 
     def setup_environment(self):
