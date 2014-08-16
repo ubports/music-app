68060fa664ad43c4d5e9005601af0b9e7eb27686
Victor Thompson <victor.thompson@gmail.com>
2014-08-15 21:13:43 -0500

    * Fix getPlaylistCovers functions to return only unique covers

--- playlists.js
+++ playlists.js
@@ -264,7 +264,8 @@ function getPlaylistCovers(playlist, max) {
                     album: rs.rows.item(i).album
                 }
 
-                if (res.indexOf(row) === -1) {
+                if (find(res, "author", rs.rows.item(i).author) === null &&
+                    find(res, "album", rs.rows.item(i).album) === null) {
                     res.push(row)
                 }
             }
@@ -274,6 +275,17 @@ function getPlaylistCovers(playlist, max) {
     }
 
     return res
+}
+
+function find(arraytosearch, key, valuetosearch) {
+
+    for (var i = 0; i < arraytosearch.length; i++) {
+
+        if (arraytosearch[i][key] == valuetosearch) {
+            return i;
+        }
+    }
+    return null;
 }
 
 function renamePlaylist(from, to) {
