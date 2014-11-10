e6d7d034fd78c2b6f86af47f31ad988e0754fa47
Victor Thompson <victor.thompson@gmail.com>
2014-11-09 18:11:31 -0600

    * Use art property for playlists

--- playlists.js
+++ playlists.js
@@ -261,8 +261,7 @@ function getPlaylistCovers(playlist, max) {
             for (var i = 0; i < rs.rows.length
                  && i < (max || rs.rows.length); i++) {
                 var row = {
-                    author: rs.rows.item(i).author,
-                    album: rs.rows.item(i).album
+                    art: musicStore.lookup(rs.rows.item(i).filename).art
                 }
 
                 if (find(res, row) === null) {
